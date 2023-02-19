68bf8b9dd347150b907b5c5689e9ff98195dbe98
dmsWide <dmswide@163.com>
2023-02-19 14:21:23 +0800

    second commit

--- juc_1/src/ThreadUnsafeExample.java
+++ juc_1/src/ThreadUnsafeExample.java
@@ -12,20 +12,21 @@ public class ThreadUnsafeExample {
     public int get() {
         return cnt;
     }
-    public static void main(String[] args) throws InterruptedException {
-    final int threadSize = 1000;
-    ThreadUnsafeExample example = new ThreadUnsafeExample();
-    final CountDownLatch countDownLatch = new CountDownLatch(threadSize);
-    ExecutorService executorService = Executors.newCachedThreadPool();
-    for (int i = 0; i < threadSize; i++) {
-        executorService.execute(() -> {
-            example.add();
-            countDownLatch.countDown();
-        });
-    }
-    countDownLatch.await();
-    executorService.shutdown();
-    System.out.println(example.get());
-    }
 
+    public static void main(String[] args) throws InterruptedException {
+        final int threadSize = 1000;
+        ThreadUnsafeExample example = new ThreadUnsafeExample();
+        final CountDownLatch countDownLatch = new CountDownLatch(threadSize);
+        ExecutorService executorService = Executors.newCachedThreadPool();
+        for (int i = 0; i < threadSize; i++) {
+            executorService.execute(() -> {
+                example.add();
+                countDownLatch.countDown();
+            });
+        }
+        countDownLatch.await();
+        executorService.shutdown();
+        System.out.println(example.get());
+        System.out.println(" ");
+    }
 }
